8f381d69a70eed8e7f4430d0cd54dd86147c8a77
Arlei <arleicarbonato@gmail.com>
2022-12-20 20:28:09 -0300

    inicio

---
+++ Matricula De Aluno/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Matricula_De_Aluno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnIdentificarCategoria = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.dtpDataDeNascimento = new System.Windows.Forms.DateTimePicker();
+            this.lblHoje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Nome: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "&Ano Nascimento: ";
+            // 
+            // btnIdentificarCategoria
+            // 
+            this.btnIdentificarCategoria.Location = new System.Drawing.Point(15, 119);
+            this.btnIdentificarCategoria.Name = "btnIdentificarCategoria";
+            this.btnIdentificarCategoria.Size = new System.Drawing.Size(220, 23);
+            this.btnIdentificarCategoria.TabIndex = 2;
+            this.btnIdentificarCategoria.Text = "Identificar Categoria";
+            this.btnIdentificarCategoria.UseVisualStyleBackColor = true;
+            this.btnIdentificarCategoria.Click += new System.EventHandler(this.btnIdentificarCategoria_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(59, 21);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(508, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Categoria";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.BackColor = System.Drawing.Color.Cornsilk;
+            this.lblCategoria.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCategoria.Location = new System.Drawing.Point(358, 119);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(209, 23);
+            this.lblCategoria.TabIndex = 8;
+            this.lblCategoria.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dtpDataDeNascimento
+            // 
+            this.dtpDataDeNascimento.Location = new System.Drawing.Point(109, 70);
+            this.dtpDataDeNascimento.Name = "dtpDataDeNascimento";
+            this.dtpDataDeNascimento.Size = new System.Drawing.Size(232, 20);
+            this.dtpDataDeNascimento.TabIndex = 9;
+            // 
+            // lblHoje
+            // 
+            this.lblHoje.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.lblHoje.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHoje.Location = new System.Drawing.Point(358, 70);
+            this.lblHoje.Name = "lblHoje";
+            this.lblHoje.Size = new System.Drawing.Size(209, 23);
+            this.lblHoje.TabIndex = 10;
+            this.lblHoje.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblHoje.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 170);
+            this.Controls.Add(this.lblHoje);
+            this.Controls.Add(this.dtpDataDeNascimento);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnIdentificarCategoria);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Matrícula de aluno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnIdentificarCategoria;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.DateTimePicker dtpDataDeNascimento;
+        public System.Windows.Forms.Label lblHoje;
+    }
+}
+
